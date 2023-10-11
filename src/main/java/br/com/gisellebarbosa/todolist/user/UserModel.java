2943b7c30b01ff855cefc0fd44f000f2c3115208
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-10 23:54:50 -0300

    feat: created getters and setters

--- src/main/java/br/com/gisellebarbosa/todolist/user/UserModel.java
+++ src/main/java/br/com/gisellebarbosa/todolist/user/UserModel.java
@@ -5,4 +5,31 @@ public class UserModel {
   public String username;
   public String name;
   public String password;
+
+  // setters
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  // getters
+  public String getUsername() {
+    return username;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
 }
