0185dc6c8b5ecc8c0d5062182540f71e6c3de84d
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-11 00:04:20 -0300

    refac: added lombok for getters and setters

--- src/main/java/br/com/gisellebarbosa/todolist/user/UserModel.java
+++ src/main/java/br/com/gisellebarbosa/todolist/user/UserModel.java
@@ -1,35 +1,12 @@
 package br.com.gisellebarbosa.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
   public String username;
   public String name;
   public String password;
-
-  // setters
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  // getters
-  public String getUsername() {
-    return username;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 
 }
