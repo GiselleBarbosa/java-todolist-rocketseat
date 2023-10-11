efe18573a65fff3106ab61bde69a0faf9a01a78f
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-11 00:55:30 -0300

    feat: added unique id configuration

--- src/main/java/br/com/gisellebarbosa/todolist/user/UserController.java
+++ src/main/java/br/com/gisellebarbosa/todolist/user/UserController.java
@@ -15,7 +15,16 @@ public class UserController {
 
   @PostMapping("/")
   public UserModel create(@RequestBody UserModel userModel) {
+
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      System.out.println("Este usuário ja existe");
+      return null;
+    }
+
     var userCreated = this.userRepository.save(userModel);
+
     return userCreated;
   }
 
