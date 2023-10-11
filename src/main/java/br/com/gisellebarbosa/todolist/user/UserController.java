06924605bbf977bf2fdb850b528a46461879c6ef
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-11 01:02:26 -0300

    feat: added responseEntity

--- src/main/java/br/com/gisellebarbosa/todolist/user/UserController.java
+++ src/main/java/br/com/gisellebarbosa/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.gisellebarbosa.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +16,17 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
 
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
-      System.out.println("Este usuário ja existe");
-      return null;
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este usuário ja existe");
     }
 
     var userCreated = this.userRepository.save(userModel);
 
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 
 }
