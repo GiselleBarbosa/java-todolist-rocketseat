ee8b001d1e8b0c4e4eceaecfa349b608605dbaaf
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-15 00:38:46 -0300

    feat: get tasks by userId is works

--- src/main/java/br/com/gisellebarbosa/todolist/tasks/taskController.java
+++ src/main/java/br/com/gisellebarbosa/todolist/tasks/taskController.java
@@ -1,11 +1,13 @@
 package br.com.gisellebarbosa.todolist.tasks;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,10 +37,18 @@ public class TaskController {
     if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body("A data de início deve ser menor que a data de término atual");
-
     }
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+    return tasks;
+
   }
 }
